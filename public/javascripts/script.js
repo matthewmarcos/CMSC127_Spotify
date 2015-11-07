fc3e86743b391d172e013c1867d4ae6b1067f90e
WareAnj <ware.angelicap@gmail.com>
2015-11-07 23:37:48 +0800

    Added signup initial design and edited script.js by adding jQuery functions

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,1 +1,13 @@
-console.log('Script.js has loaded')
+$(document).ready(function(){
+  $("#signup").hide();
+  $("#bodyButton").click(function(){
+    $("#home").hide();
+    $("#signup").show();
+  });
+});
+
+$('.datepicker').pickadate({
+  selectMonths: true, // Creates a dropdown to control month
+  selectYears: 15 // Creates a dropdown of 15 years to control year
+});
+      
