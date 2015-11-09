25a2dbb5948d7699dc8a01f6f342ae193a0acb9a
Matthew <matthewmarcos94@gmail.com>
2015-11-09 23:36:08 +0800

    Initial database connections working

---
+++ bin/databaseMaster.js
@@ -0,0 +1,29 @@
+/*
+	databaseMaster:
+		Create tables if does not exist in schema.
+		Creates admin seed afterwards.
+*/
+
+var pg = require('pg');
+module.exports = function() {
+
+	var conString = "postgres://app_ro:myPassword@localhost/test1";
+	var client = new pg.Client(conString);
+
+	client.connect(function(err) {
+
+		if(err) {
+			return console.error('could not connect to postgres', err);
+		}
+		
+		client.query('SELECT NOW() AS "theTime"', function(err, result) {
+			if(err) {
+				return console.error('error running query', err);
+			}
+			console.log(result.rows[0].theTime);
+			//output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+			console.log('Database connection successful');
+			client.end();
+		});
+	});
+};
