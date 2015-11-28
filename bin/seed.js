75ddd4306d1ed9b0ee051d88323df06551fbb14a
Matthew <matthewmarcos94@gmail.com>
2015-11-28 16:20:31 +0800

    Database seeding added. npm run seed

--- bin/seed.js
+++ bin/seed.js
@@ -4,16 +4,48 @@
 var pg = require('pg');
 var async = require('async');
 var dbUrl = "postgres://cmsc127spotify:cmsc127@localhost/spotify";
-var data = require('./../models/seed').data;
+var queries = require('./../models/seed').queries;
+var i = 0;
 
 var fakeQuery = function(string, callback) {
 	callback(null, string);
 };
 
-	async.eachSeries(data, 
-	function(datum, callback) {
-		console.log(datum);
-		callback(null);
+	// queries.forEach(function(queryString) {
+	//     pg.connect(dbUrl, function(err, client) {
+	//         if(err) {
+	//             return console.error('Client cannot connect to PG');
+	//         }
+	//         // res.send('Updating at ' + req.params.id);
+	//         client.query(queryString, function(err, data){
+	//             client.end();
+	//             if(err) {
+	//                 console.log('Error');
+	//                 return;
+	//             }
+	//         });
+	//     });  
+
+	// });
+
+	async.eachSeries(queries, 
+	function(queryString, callback) {
+		i++;
+		pg.connect(dbUrl, function(err, client) {
+	        if(err) {
+	            return console.error('Client cannot connect to PG');
+	        }
+	        // res.send('Updating at ' + req.params.id);
+	        client.query(queryString, function(err, data){
+	            client.end();
+	            if(err) {
+	                console.log('Error' + i);
+	                // callback(err);
+	                // return;
+	            }
+	            callback(null);
+	        });
+	    });  
 	}, function(err) {
 		console.log('Completed bro');
 	});
