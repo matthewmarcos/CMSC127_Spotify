ea24cd816a74cba72b9b61bc03140bc66a885438
Matthew <matthewmarcos94@gmail.com>
2015-11-28 12:32:14 +0800

    Database queries and database change

--- sampleInsertQueries.sql
+++ sampleInsertQueries.sql
@@ -3,28 +3,16 @@
 ------------------------------------------------------------------------------------------------------------------------------
 --users
 --1
-insert into users(username,	password, email, isApproved, isAdmin, dateApproved)VALUES('Diego','Sevilla','diegosevilla@gmail.com',false,false,now());
+insert into users(username, fname, lname,password, email, isApproved, isAdmin, dateApproved)VALUES('Diego','Diego','Sevilla','Sevilla','diegosevilla@gmail.com',false,false,now());
 --2
-insert into users(username,	password, email, isApproved, isAdmin, dateApproved)VALUES('homer','malijan','homermalijan@gmail.com',false,false,now());
+insert into users(username, fname, lname, password, email, isApproved, isAdmin, dateApproved)VALUES('homer','homer','malijan','malijan','homermalijan@gmail.com',false,false,now());
 --3
-insert into users(username,	password, email, isApproved, isAdmin, dateApproved)VALUES('Pauline','Payawal','paulinepayawal@gmail.com',false,false,now());
+insert into users(username, fname, lname, password, email, isApproved, isAdmin, dateApproved)VALUES('Pauline','Pauline','Payawal','Payawal','paulinepayawal@gmail.com',false,false,now());
 --4
-insert into users(username,	password, email, isApproved, isAdmin, dateApproved)VALUES('Carol','Adajar','caroladajar@gmail.com',false,false,now());
+insert into users(username, fname, lname, password, email, isApproved, isAdmin, dateApproved)VALUES('Carol','Carol','Adajar','Adajar','caroladajar@gmail.com',false,false,now());
 --5
-insert into users(username,	password, email, isApproved, isAdmin, dateApproved)VALUES('MJ','Constantino','mjconstantino@gmail.com',false,false,now());
-------------------------------------------------------------------------------------------------------------------------------
---users_name
---1
-insert into users_name(users_id,fname,lname)VALUES(1,'Diego','Sevilla');
---2
-insert into users_name(users_id,fname,lname)VALUES(2,'Homer','Malijan');
---3
-insert into users_name(users_id,fname,lname)VALUES(3,'Pauline','Payawal');
---4
-insert into users_name(users_id,fname,lname)VALUES(4,'Carol','Adajar');
---5
-insert into users_name(users_id,fname,lname)VALUES(5,'MJ','Constantino');
-------------------------------------------------------------------------------------------------------------------------------
+insert into users(username, fname, lname, password, email, isApproved, isAdmin, dateApproved)VALUES('MJ','MJ','Constantino','Constantino','mjconstantino@gmail.com',false,false,now());
+	------------------------------------------------------------------------------------------------------------------------------
 --playlist
 --1
 insert into playlist(users_id,playlistname,date_created)VALUES(1,'Diego Playlist',now());
@@ -39,15 +27,21 @@ insert into playlist(users_id,playlistname,date_created)VALUES(5,'MJ Playlist',n
 ------------------------------------------------------------------------------------------------------------------------------
 --music
 --1
-insert into music(file_id, music_length, times_played, views)VALUES('/../music/shakeItOff.mp3',360,42,51);
+insert into music(file_path,music_title, music_length, times_played, views)VALUES('/../music/shakeItOff.mp3','Shake It Off',360,42,51);
 --2
-insert into music(file_id, music_length, times_played, views)VALUES('/../music/latch.mp3',407,39,72);
+insert into music(file_path,music_title, music_length, times_played, views)VALUES('/../music/latch.mp3','Latch',407,39,72);
 --3
-insert into music(file_id, music_length, times_played, views)VALUES('/../music/MarvinGaye.mp3',3230,32,69);
+insert into music(file_path,music_title, music_length, times_played, views)VALUES('/../music/MarvinGaye.mp3','Marvin Gaye',3230,32,69);
 --4
-insert into music(file_id, music_length, times_played, views)VALUES('/../music/SmokeAndMirrors.mp3',390,89,96);
+insert into music(file_path,music_title, music_length, times_played, views)VALUES('/../music/SmokeAndMirrors.mp3','Smoke and Mirrors',390,89,96);
 --5
-insert into music(file_id, music_length, times_played, views)VALUES('/../music/Sorry.mp3',362,92,109);
+insert into music(file_path,music_title, music_length, times_played, views)VALUES('/../music/Decode','Decode',390,89,96);
+insert into music(file_path,music_title, music_length, times_played, views)VALUES('/../music/Chinito','Chinito',390,89,96);
+insert into music(file_path,music_title, music_length, times_played, views)VALUES('/../music/Alright','Alright',390,89,96);
+insert into music(file_path,music_title, music_length, times_played, views)VALUES('/../music/BrickByBoringBrick','Brick By Boring Brick',390,89,96);
+insert into music(file_path,music_title, music_length, times_played, views)VALUES('/../music/','',390,89,96);
+insert into music(file_path,music_title, music_length, times_played, views)VALUES('/../music/','',390,89,96);
+
 ------------------------------------------------------------------------------------------------------------------------------
 --artist
 --1
@@ -63,15 +57,15 @@ insert into artist(artist_name,artist_photo)VALUES('Justin BAEber','/../Artist_P
 ------------------------------------------------------------------------------------------------------------------------------
 --album
 --1
-insert into album(artist_id,year,album_name,subscriber)VALUES(1,2015,'The 1989',34242);
+insert into album(artist_id,year,album_name)VALUES(1,2015,'The 1989');
 --2
-insert into album(artist_id,year,album_name,subscriber)VALUES(2,2015,'In The Lonely Hour',23241);
+insert into album(artist_id,year,album_name)VALUES(2,2015,'In The Lonely Hour');
 --3
-insert into album(artist_id,year,album_name,subscriber)VALUES(3,2015,'huehue',1231431);
+insert into album(artist_id,year,album_name)VALUES(3,2015,'huehue');
 --4
-insert into album(artist_id,year,album_name,subscriber)VALUES(4,2015,'Smoke And Mirrors',34242);
+insert into album(artist_id,year,album_name)VALUES(4,2015,'Smoke And Mirrors');
 --5
-insert into album(artist_id,year,album_name,subscriber)VALUES(5,2015,'Purpose',923481);
+insert into album(artist_id,year,album_name)VALUES(5,2015,'Purpose');
 ------------------------------------------------------------------------------------------------------------------------------
 --artist-music relation
 --1
