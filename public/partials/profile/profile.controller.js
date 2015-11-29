e9e7941aa70183f366befeb6e1ece1740827b235
Matthew <matthewmarcos94@gmail.com>
2015-11-30 01:33:23 +0800

    Added basic shit in profile page. Please add editing functionality

--- public/partials/profile/profile.controller.js
+++ public/partials/profile/profile.controller.js
@@ -7,16 +7,12 @@
 		ProfileCtrl.$inject = ["$scope","$http"];
 
 		function ProfileCtrl($scope,$http){
-			$scope.username = '';
-			$scope.picture = '';
-			$scope.isadmin = false;
+			$scope.user = {};
 	
 			$http.get('/profile').then(function(data){
 				// alert('getinfo');
 				// console.log(data.data.username);
-				$scope.username = data.data.username;
-				$scope.picture = data.data.picture;
-				$scope.isadmin = data.data.isadmin;
+				$scope.user = data.data;
 
 			}, function(err) {
 				console.log(err);
