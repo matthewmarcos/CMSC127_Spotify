6b0f6f2405f566ce2be33289c0bb0bff18917171
hoommaah <hcmalijan@up.edu.ph>
2015-11-30 17:16:11 +0800

    add music

--- public/partials/music/music.controller.js
+++ public/partials/music/music.controller.js
@@ -14,7 +14,29 @@
 					console.log(data);
 					$scope.tracks = data.data;
 				}, function(err) {
-					console.log("errorewrwewr");
+					console.err(err);
 				});
+				
+			$scope.addTrackTitle = '';
+			$scope.addTrackArtist = '';
+			$scope.addTrackLength = '';
+			
+			$scope.addMusic = function() {
+
+				$http.post('/music', {
+					music_title: $scope.addTrackTitle,
+					music_length: $scope.addTrackLength
+				}).then(function(data){
+					alert('Successfully Uploaded');
+					$scope.addTrackTitle = '';
+					$scope.addTrackArtist = '';
+					$scope.addTrackLength = '';
+				}, function(err) {
+					alert("Invalid");
+					$scope.addTrackTitle = '';
+					$scope.addTrackArtist = '';
+					$scope.addTrackLength = '';
+				});
+			};
 		}
 })();
