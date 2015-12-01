b844de5155fd075348b707cffe74f039a21c31dd
Matthew <matthewmarcos94@gmail.com>
2015-12-01 20:38:19 +0800

    Edited the playlist

--- public/partials/playlist/playlist.controller.js
+++ public/partials/playlist/playlist.controller.js
@@ -22,11 +22,15 @@
 				
 			}
 
+			$scope.remove = function (music_id) {
+				console.log(music_id);
+			};
+
 			$scope.add = function(music_id) {
 				console.log(music_id);
-				console.log($scope.musicList[theIndexOf(music_id)])
-				$scope.toCreate.push($scope.musicList[theIndexOf(music_id)]);
-			}
+				console.log($scope.musicList[theIndexOf(music_id, $scope.musicList)]);
+				$scope.toCreate.push($scope.musicList[theIndexOf(music_id, $scope.musicList)]);
+			};
 
 			$scope.search = function() {
 				// console.log($scope.query);
@@ -39,9 +43,9 @@
 					});
 			};
 
-			var theIndexOf = function(music_id) {
-				for(var i = 0 ; $scope.musicList.length ; i++) {
-					if($scope.musicList[i].music_id === music_id) {
+			var theIndexOf = function(music_id, array) {
+				for(var i = 0 ; array.length ; i++) {
+					if(array[i].music_id === music_id) {
 						return i;
 					}
 				}
