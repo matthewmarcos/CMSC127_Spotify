b18878882687e0d7af587cded158696d4bc641fa
Matthew <matthewmarcos94@gmail.com>
2015-12-02 12:04:58 +0800

    AlbumView

--- public/partials/AlbumView/albumView.controller.js
+++ public/partials/AlbumView/albumView.controller.js
@@ -10,12 +10,12 @@
 			$scope.albumView = [];
 			alert($routeParams.album_id);
 			console.log('routeParams is ' + $routeParams.album_id);
-/*			$http.get('/album/music/'  + $routeParams.album_id)
+			$http.get('/album/music/'  + $routeParams.album_id)
 				.then(function(data) {
 					console.log(data.data);
 					$scope.albumView = data.data;
 				}, function(err) {
 					console.err(err);
-				});		*/
+				});		
 		}
 })();
