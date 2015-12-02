cf5e98f979ddd142940232430a77a158ea56e388
hoommaah <hcmalijan@up.edu.ph>
2015-12-02 12:49:36 +0800

    fixed header

--- public/partials/AlbumView/albumView.controller.js
+++ public/partials/AlbumView/albumView.controller.js
@@ -8,12 +8,14 @@
 
 		function AlbumViewCtrl($scope, $http, $routeParams){
 			$scope.albumView = [];
+			$scope.albumName = '';
 			alert($routeParams.album_id);
 			console.log('routeParams is ' + $routeParams.album_id);
 			$http.get('/album/music/'  + $routeParams.album_id)
 				.then(function(data) {
 					console.log(data.data);
 					$scope.albumView = data.data;
+					$scope.albumName = $scope.albumView[0].album_name;
 				}, function(err) {
 					console.err(err);
 				});		
